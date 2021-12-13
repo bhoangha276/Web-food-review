23010876cc269f006441ab187ad6f9dad7921f03
HaHoang <82991310+bhoangha276@users.noreply.github.com>
2021-12-13 22:00:47 +0700

    Post

---
+++ FoodReviewWeb/Areas/Admin/Controllers/PostsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FoodReviewWeb.Models.Framework;
+
+namespace FoodReviewWeb.Areas.Admin.Controllers
+{
+    public class PostsController : Controller
+    {
+        private FoodReviewEntities1 db = new FoodReviewEntities1();
+
+        // GET: Admin/Posts
+        public ActionResult Index()
+        {
+            var post = db.Post.Include(p => p.Account);
+            return View(post.ToList());
+        }
+
+        // GET: Admin/Posts/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Post.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // GET: Admin/Posts/Create
+        public ActionResult Create()
+        {
+            ViewBag.Author = new SelectList(db.Account, "ID", "Username");
+            return View();
+        }
+
+        // POST: Admin/Posts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Author,Title,Contents,Image,Slug,LikeCount,CreateAt,UpdateAt")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Post.Add(post);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Author = new SelectList(db.Account, "ID", "Username", post.Author);
+            return View(post);
+        }
+
+        // GET: Admin/Posts/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Post.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Author = new SelectList(db.Account, "ID", "Username", post.Author);
+            return View(post);
+        }
+
+        // POST: Admin/Posts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Author,Title,Contents,Image,Slug,LikeCount,CreateAt,UpdateAt")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(post).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Author = new SelectList(db.Account, "ID", "Username", post.Author);
+            return View(post);
+        }
+
+        // GET: Admin/Posts/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Post.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // POST: Admin/Posts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Post post = db.Post.Find(id);
+            db.Post.Remove(post);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

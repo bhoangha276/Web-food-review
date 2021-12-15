67079db1a48ae5c1a43883ad49a05ee24a11bcc6
HaHoang <82991310+bhoangha276@users.noreply.github.com>
2021-12-15 17:11:37 +0700

    postdetail

---
+++ FoodReviewWeb/Controllers/PostDetailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodReviewWeb.Controllers
+{
+    public class PostDetailController : Controller
+    {
+        // GET: PostDetail
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

3d4b644a164f2b55147d1bb04d1a06c2a4b25303
HaHoang <82991310+bhoangha276@users.noreply.github.com>
2021-12-16 16:34:13 +0700

    detail post

--- FoodReviewWeb/Controllers/ReviewController.cs
+++ FoodReviewWeb/Controllers/ReviewController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using FoodReviewWeb.Models.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,20 +10,30 @@ namespace FoodReviewWeb.Controllers
 {
     public class ReviewController : Controller
     {
+        private FoodReviewEntities1 db = new FoodReviewEntities1();
+
         // GET: Review
         public ActionResult Index()
         {
             ViewBag.Message = "Your Review page.";
 
-            var lst = new List<Models.Framework.Post>();
-            Models.Framework.FoodReviewEntities1 db = new Models.Framework.FoodReviewEntities1();
+            var lst = new List<Post>();
             lst = db.Post.OrderByDescending(x => x.ID).ToList();
 
             return View(lst);
         }
-        public ActionResult Detail()
+        public ActionResult Detail(string id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Account.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
         }
     }
 }
